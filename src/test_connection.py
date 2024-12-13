c5968b2df4df9b93989f220640268a436a17c4d8
DAKudryashev <145067538+DAKudryashev@users.noreply.github.com>
2024-12-13 19:35:30 +0300

    completed class to connect to forte

--- src/test_connection.py
+++ src/test_connection.py
@@ -1,51 +1,60 @@
 import socket
 import os
-
-# Чтение fboot файла
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-file_path = os.path.join(parent_dir, 'testProject_FORTE_PC.fboot')
-print("Путь к файлу:", file_path)
-
-file = open(file_path, 'r')
-
-# Создание сокета
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Подключение к серверу
-server_address = ('localhost', 61499)
-client_socket.connect(server_address)
-
-# Отправка данных
-# while True:
-#     message = file.readline()
-#     if not message:
-#         break
-#     client_socket.sendall(message.encode())
-#     # Получение ответа
-#     try:
-#         client_socket.settimeout(10.0)
-#         response = client_socket.recv(2)
-#         print(f'Received: {response.decode()}')
-#
-#     except socket.timeout:
-#         print("Таймаут: ожидание данных...")
-#         continue
-
-for i in range(1):
-    message = '<Request ID="0" Action="QUERY"><FB NAME="*" Type="*"/></Request>'
-    if not message:
-        break
-    client_socket.sendall(message.encode())
-    # Получение ответа
-    try:
-        client_socket.settimeout(10.0)
-        response = client_socket.recv(2)
-        print(f'Received: {response.decode()}')
-
-    except socket.timeout:
-        print("Таймаут: ожидание данных...")
-        continue
+import struct
 
 
-# Закрытие сокета
-client_socket.close()
+class TcpFileSender():
+    def __init__(self, file_name):
+        # Создание сокета
+        self.file_name = file_name
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def set_connection(self):
+        # Подключение к серверу
+        server_address = ('localhost', 61499)
+        self.client_socket.connect(server_address)
+
+    def disconnect(self):
+        # Закрытие сокета
+        self.client_socket.close()
+
+    def send_fboot(self):
+        # Чтение fboot файла
+        parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+        file_path = os.path.join(parent_dir, self.file_name)
+        print("File path:", file_path)
+        file = open(file_path, 'r')
+
+        while True:
+            message = file.readline()[:-1]
+            if not message:
+                break
+
+            # Обработка строки с сообщением
+            separator = message.find(';')
+            res_name = message[:separator]
+            xml_com = message[separator + 1:]
+            res_name_length = struct.pack('h', separator)[::-1]
+            xml_com_length = struct.pack('h', len(xml_com))[::-1]
+            message = '\x50' + res_name_length.decode() + res_name + '\x50' + xml_com_length.decode() + xml_com
+            # print(message)
+
+            # Отправка данных
+            self.client_socket.sendall(message.encode())
+
+            # Получение ответа
+            try:
+                self.client_socket.settimeout(10.0)
+                response = self.client_socket.recv(1024)
+                print(f'Received: {response.decode()}\n')
+
+            except socket.timeout:
+                print("Таймаут: ожидание данных...")
+                continue
+
+
+if __name__ == '__main__':
+    tcp_file_sender = TcpFileSender('k.fboot')
+    tcp_file_sender.set_connection()
+    tcp_file_sender.send_fboot()
+    tcp_file_sender.disconnect()
